4b6c47789582c9dc64c04a4247b217e81bbd735b
Franklin Anahua <franklinanahua5@gmail.com>
2024-08-25 12:31:07 -0500

    3° clase, operaciones aritmeticos de python

---
+++ 3) op_aritmeticos.py
@@ -0,0 +1,12 @@
+a=5
+b=3
+suma = a + b
+resta = a - b
+multiplicar = a*b
+dividir = a/b #devuelve siempre flotantes
+potencia = a**b
+division_baja = a//b #enteros  int redondeo  hacia abajo
+resto = a%b #resto
+
+tipo_de_dato = type(division_baja)#type(dato) indica que tipo de dato es.
+print(tipo_de_dato)
