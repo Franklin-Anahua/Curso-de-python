8228866210c218e1e509ecfedaca63a9e8433314
Franklin Anahua <franklinanahua5@gmail.com>
2024-08-25 12:29:43 -0500

    2° clase, datos compuestos de python

---
+++ 2) datos_compuestos.py
@@ -0,0 +1,9 @@
+lista = ["frank", 15, True] #se pude modificar la lista
+tupla = ("frank", 15, False) # igual que el anterios pero no se modifica    
+conjunto = {"frank", 15, False} # no repite datos
+diccionario = {
+    "nombre" : "Franklin",
+    "edad" : "17",
+    "estado" : "soltero"
+} #aqui es key codigo y luego dato 
+print(conjunto)
