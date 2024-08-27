963ca9307b437906eb273c9277ef5b7baeb8a4b0
Franklin Anahua <franklinanahua5@gmail.com>
2024-08-26 20:18:44 -0500

    6° clase, metodos de lista de python

---
+++ 6)metodos_listas.py
@@ -0,0 +1,14 @@
+
+#LIST - crea una lista
+lista = list(["hola","Franklin",34])
+
+#LEN - cuenta la cantidad de elementos de una lista
+#APPEND - agrega un elemento a la lista
+#INSERT - agrega un elemento a la lista en el indice especificado
+#EXTEND - agrega varios elementos a la lista
+#POP - elimina un elemento de una lista, pide indice y devuelve valor
+#REMOVE - remueve un elemento de una lista, pide valor
+#CLEAR - elimina todos los elementos de una lista, pide valor
+#SORT - ordena una lista de forma ascendente a descendente
+#REVERSE - invierte los elementos de una lista
+print(lista)
