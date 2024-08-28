e8113436964823e72aa9dce9c58feeef626b5474
Franklin Anahua <franklinanahua5@gmail.com>
2024-08-27 20:45:46 -0500

    actualizacion

--- Numeros_Perfectos.py
+++ Numeros_Perfectos.py
@@ -1,12 +1,1 @@
-#Un número perfecto es un número entero positivo que es igual a la suma de sus divisores 
-#propios positivos. Este concepto está relacionado con los números primos de Mersenne, y 
-# se pueden generar usando la fórmula
-#2**(p−1) * (2**p−1), donde p y 2**p−1son primos.
-a = {6, 28, 496, 8128, 2096128}
-p = 11
-
-#Generando número perfecto
-b = 2**(p-1) * (2**p -1)
-print(b)
-
-#objetivo encontrar el numero perfecto impar
+#arboles de algoritmo
