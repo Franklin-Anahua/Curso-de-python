ec5c6ef741205f2d3c5b3164a33d7eddbe734e60
Franklin Anahua <franklinanahua5@gmail.com>
2024-08-28 19:37:09 -0500

    actualizacion

--- Numeros_Perfectos.py
+++ Numeros_Perfectos.py
@@ -1,1 +1,46 @@
 #arboles de algoritmo
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.value = key
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        if self.root is None:
+            self.root = Node(key)
+        else:
+            self._insert_recursively(self.root, key)
+
+    def _insert_recursively(self, node, key):
+        if key < node.value:
+            if node.left is None:
+                node.left = Node(key)
+            else:
+                self._insert_recursively(node.left, key)
+        else:
+            if node.right is None:
+                node.right = Node(key)
+            else:
+                self._insert_recursively(node.right, key)
+
+    def inorder_traversal(self, node):
+        if node:
+            self.inorder_traversal(node.left)
+            print(node.value, end=' ')
+            self.inorder_traversal(node.right)
+
+# Ejemplo de uso
+if __name__ == "__main__":
+    tree = BinaryTree()
+    tree.insert(5)
+    tree.insert(3)
+    tree.insert(7)
+    tree.insert(2)
+    tree.insert(4)
+
+    print("Recorrido en orden del árbol binario:")
+    tree.inorder_traversal(tree.root)
