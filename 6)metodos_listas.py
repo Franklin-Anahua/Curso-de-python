67e1133015695e3ba3d4458ff71ebcb9210c518b
Franklin Anahua <franklinanahua5@gmail.com>
2024-09-02 19:48:51 -0500

    actualizacion

--- 6)metodos_listas.py
+++ 6)metodos_listas.py
@@ -3,12 +3,23 @@
 lista = list(["hola","Franklin",34])
 
 #LEN - cuenta la cantidad de elementos de una lista
+lista1 = len(lista)
+
 #APPEND - agrega un elemento a la lista
+lista.append("Bienvenido")
+
 #INSERT - agrega un elemento a la lista en el indice especificado
+lista.insert(0, "hello")
+
 #EXTEND - agrega varios elementos a la lista
+lista.extend(["como", "te", "va"])
+
 #POP - elimina un elemento de una lista, pide indice y devuelve valor
+lista.pop(-1, -2)
+
 #REMOVE - remueve un elemento de una lista, pide valor
 #CLEAR - elimina todos los elementos de una lista, pide valor
 #SORT - ordena una lista de forma ascendente a descendente
 #REVERSE - invierte los elementos de una lista
+
 print(lista)
