06fbb1fa64b8a3a6bb9065cc211a8d5fa413f397
Franklin Anahua <franklinanahua5@gmail.com>
2024-09-03 19:07:55 -0500

    actuallity

--- 8)Entrada_datos.py
+++ 8)Entrada_datos.py
@@ -1,4 +1,9 @@
+#Cadena de nombres
 nombre=input("Nombre: ")
-
 print(f"Bienvenido {nombre}")
 print("Bienvenido ",nombre)
+#numerico
+numero=input("Numero: ")
+resultado=numero*2
+resultado=int(numero)*2
+resultado=float(resultado)*1.5
