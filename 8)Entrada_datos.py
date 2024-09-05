23b25f90f5a1a7f58b31142200a20a7e4d268c86
Franklin Anahua <franklinanahua5@gmail.com>
2024-09-04 21:18:11 -0500

    actuallity

--- 8)Entrada_datos.py
+++ 8)Entrada_datos.py
@@ -10,3 +10,8 @@ resultado=int(numero)*2
 print(resultado)
 resultado=float(numero)*1.5
 print(resultado)
+
+#ejemplo "EVAL()" Evalúa la expresión ingresada como código Python.
+num=input("Ingrese un operador:")
+rpta=eval(num)
+print(rpta)
