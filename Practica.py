83b2a43a5f591501286144adf7e7f0efa98cd528
Franklin Anahua <franklinanahua5@gmail.com>
2024-09-06 22:05:30 -0500

    actuallity

--- Practica.py
+++ Practica.py
@@ -51,3 +51,13 @@ while rep:
  
 num=eval(input())
 print(num)
+
+#Matrices y estructuras
+
+matriz=[[1,2,3],
+        [4,5,6],
+        [7,8,9]]
+for fila in matriz:
+    for elemento in fila:
+        print(elemento, end=" ")
+    print()
