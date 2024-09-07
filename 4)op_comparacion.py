d48f94696632636622b206033bed9872f13a4b35
Franklin Anahua <franklinanahua5@gmail.com>
2024-09-06 22:04:08 -0500

    actuallity

--- 4)op_comparacion.py
+++ 4)op_comparacion.py
@@ -1,6 +1,5 @@
 usuario = "Franklin"
-print("Ingresa tu Nombre de usuario")
-usuar = input()
+usuar = input("Ingresa tu Nombre de usuario: ")
 
 # os.system("cls") # para windows
 while usuar != "Franklin":
@@ -8,3 +7,16 @@ while usuar != "Franklin":
     print("Intente de nuevo")
     usuar = input()
 print("se ha ingresado correctamente el usuario")
+
+#Año bisiesto
+op=True
+while op:
+    fecha=int(input("Ingrese el año(bisiesto o no): "))
+    if fecha == 0:
+        print("Saliendo...")
+        op=False
+    elif (fecha%4==0 and fecha%100==0) or fecha%400==0:
+        print(f"{fecha} es un año bisiesto")
+    else:
+        print(f"{fecha} no es un año bisiesto")
+    
