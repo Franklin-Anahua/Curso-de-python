31a59db071c061205fe57511a0c7de74d3e2d92e
Franklin Anahua <franklinanahua5@gmail.com>
2024-09-06 22:04:27 -0500

    actuallity

--- 7)metodos_diccionario.py
+++ 7)metodos_diccionario.py
@@ -9,10 +9,10 @@ claves = diccionario.keys()
 #devuelve el valor de la clave
 valor1 = diccionario["Primarca"]
 valor2 = diccionario.get("mo hay nad")
-#elimina todo los elementos
-#diccionario.clear()
 #elimimina un elemento
 elim=diccionario.pop("Primarca")
 #obtiene un elemento iterable
 iterable=diccionario.items()
 print(iterable)
+#elimina todo los elementos
+diccionario.clear()
