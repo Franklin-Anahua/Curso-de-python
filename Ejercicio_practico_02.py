695578d3a53fe62ec3725b81a8f77dba23d9cf3c
Franklin Anahua <franklinanahua5@gmail.com>
2024-09-09 20:43:58 -0500

    actuallity

--- Ejercicio_practico_02.py
+++ Ejercicio_practico_02.py
@@ -7,9 +7,16 @@
 #c) Dhor habla un 30% mas rapido:
 #¿Cuanto tardara en decirlo?
 
+#pides una frase
 frase=input("Escribe una frase, y te calcule el tiemo que te tardarias: ")
+#creamos una lista
 p_separadas=frase.split(" ")
+#len() cantidad de elementos
 cantidad_p=len(p_separadas)
+#cuanto tiempo demora
 print(f"""
 Dijistes {cantidad_p} palabras, tardarias {cantidad_p/2} segundos en decirlo
 Dhor lo diria en {(cantidad_p/2*1.3)*100//1/100} segundos en decirlo""")
+#en caso de que tarde mas de un minuto
+if cantidad_p > 120:
+    print("para flaco no te pedi que escribieras tu testamento...")
