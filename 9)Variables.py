2ba061b89891562fc3619fec8e32c9c1358b47ea
Franklin Anahua <franklinanahua5@gmail.com>
2024-09-09 20:43:19 -0500

    9° clase, variables 2.0 de python

---
+++ 9)Variables.py
@@ -0,0 +1,19 @@
+#Creando a la tupla
+d_tupla = ("Dhor", "Adous", 18)
+d_lista = ["Dhor", "Adous", 18]
+#desempaquetado
+nombre,apellido,edad = d_tupla
+nombre,apellido,edad = d_lista
+#mostrando resultado
+print(f"Mi nombre es {nombre} {apellido} con {edad} años")
+#desempaquetado
+nombre,apellido,edad = d_lista
+
+
+#Creando tuplas
+tuplas = tuple(["dato1", "dato2"])
+tuplas1 = "Dato1", "Dato2"
+tuplas2 = "Dato",
+print(tuplas2, "\n")
+
+#Creando conjunto
