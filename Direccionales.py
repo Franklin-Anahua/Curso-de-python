64185182428b7df00e4fe8aca69794637b655c43
Franklin Anahua <franklinanahua5@gmail.com>
2024-09-12 20:49:11 -0500

    actuallity

--- Direccionales.py
+++ Direccionales.py
@@ -1,30 +1,13 @@
-
-import keyboard
-
-colores = ["Rojo", "Verde", "Azul", "Amarillo", "Blanco", "Negro"]
-indice = 0  # Índice del color seleccionado
-
-def mostrar_color_actual():
-    print("\nElige un color usando las teclas de flecha. Pulsa 'q' para salir.")
-    for i, color in enumerate(colores):
-        if i == indice:
-            print(f"> {color}")  # Indicar el color seleccionado
-        else:
-            print(f"  {color}")
-
-mostrar_color_actual()
-
-while True:
-    if keyboard.is_pressed('up'):
-        if indice > 0:
-            indice -= 1
-        mostrar_color_actual()
-        keyboard.wait('up')  # Espera que se suelte la tecla
-    elif keyboard.is_pressed('down'):
-        if indice < len(colores) - 1:
-            indice += 1
-        mostrar_color_actual()
-        keyboard.wait('down')  # Espera que se suelte la tecla
-    elif keyboard.is_pressed('q'):
-        print("Has salido del programa.")
-        break
+#Dirreccionales
+op=input("brujula")
+match(op):
+    case "w":
+        print("Norte")
+    case "s":
+        print("Sur")
+    case "a":
+        print("Este")
+    case "d":
+        print("Oeste")
+    case _:
+        print("No es una direccion valida")
