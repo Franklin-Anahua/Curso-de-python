f59e8e8a62482403c6cc7ce6ed214ae08317af43
Franklin Anahua <franklinanahua5@gmail.com>
2024-09-13 21:07:58 -0500

    actuallity

--- El_Dilema_del_Prisionero.py
+++ El_Dilema_del_Prisionero.py
@@ -78,7 +78,8 @@ while i < partidas:
 #def jugar_dilema_prisionero():  
     #print("Bienvenido al Dilema del Prisionero")  
     # Opciones de los jugadores  
-    # opciones = ["Cooperar", "Traicionar"]  
+    # opciones = ["Cooperar", "Traicionar"]
+#actuallity
     
 
 
